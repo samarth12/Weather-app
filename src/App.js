2d9789c47b6a4139318276abfc2f91f9bc726a31
Samarth Bhandari <samarth12@live.com>
2018-03-06 23:29:51 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "1e53ff0a8354ffc56a1620657bcbb5f3";
+
+class App extends React.Component{
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+
+  };
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    //const temoUrl = 'http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=1e53ff0a8354ffc56a1620657bcbb5f3';
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    //const api_call = await fetch(temoUrl);
+    const data = await api_call.json();
+    console.log(data);
+    if(city && country){
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name ,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: undefined
+    });
+  }else{
+    this.setState({
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: "Please enter the values!"
+    });
+  }
+}
+  render(){
+    return(
+      <div>
+      <Titles />
+      <Form getWeather = {this.getWeather}/>
+      <Weather
+      temperature = {this.state.temperature-273}
+      city = {this.state.city}
+      country = {this.state.country}
+      humidity = {this.state.humidity}
+      description = {this.state.description}
+      error ={this.state.error}
+      />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Titles extends React.Component{
+  render(){
+    return(
+      <div>
+      <h1>Weather Finder</h1>
+      <p>Find temperature</p>
+      </div>
+    );
+  }
+}
+export default Titles;
